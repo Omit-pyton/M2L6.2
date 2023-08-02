de7b405d6a4b8a589844fa70c7271d08c563c842
Omit-pyton <Omit-pyton@users.noreply.github.com>
2023-08-02 06:42:36 +0300

    Initial commit

---
+++ Homework 2.py
@@ -0,0 +1,41 @@
+# Розширте клас "Person" з попереднього завдання, додавши методи для зміни значень імені та віку.
+# Зробіть так, щоб ім'я не могло містити цифр, а вік був обмежений в діапазоні від 0 до 120.
+# При спробі встановити недійсне значення, виведіть повідомлення про помилку.
+# (приклад у презентації)
+import re
+
+
+class Person:
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
+
+    def get_name(self):
+            return self.__name
+
+    def get_age(self):
+        return self.__age
+
+    import re
+
+    def set_name(self, name):
+        if re.match(r"^\D+$", name):
+            return name
+        else:
+            return False
+
+    def set_age(self, age):
+        if age in range(120):
+            return age
+        else:
+            return False
+
+
+
+person = Person("Mike25", 305)
+
+print(person.set_name("Mike"))
+print(person.set_age(20))
+
+print(person.set_name("Mik3e"))
+print(person.set_age(250))
